afce27bef34db17b2171f09991301a0751673404
Vitalii Zelenyi <vzntst@gmail.com>
2020-08-11 22:22:26 +0300

    third commit

--- src/js/countries.js
+++ src/js/countries.js
@@ -14,25 +14,26 @@ refs.input.addEventListener('input', debounce(getCountriesHandler, 500));
 
 function getCountriesHandler(event) {
   const searchQuery = event.target.value;
-  clearCountryList();
-  countries.fetchCountries(searchQuery).then(data => buildMarkup(data));
-}
-
-let markup;
-function buildMarkup(countries) {
-  if (countries.length === 1) {
-    markup = countries.map(country => countryCardTemplate(country)).join('');
-    renderCountryRows(refs.countryList);
-  } else if (countries.length < 11) {
-    markup = countries.map(country => countryListTemplate(country)).join('');
-    renderCountryRows(refs.countryList);
-  } else {
-    return error('Too many matches found. Please enter a more specific query!');
+  if (searchQuery !== '') {
+    clearCountryList();
+    countries.fetchCountries(searchQuery).then(buildMarkup);
   }
 }
 
-function renderCountryRows(directory) {
-  directory.insertAdjacentHTML('beforeend', markup);
+function buildMarkup(countries) {
+  let markup;
+  if (countries.length === 1) {
+    markup = countries.map(country => countryCardTemplate(country)).join('');
+  } else if (countries.length < 11) {
+    markup = countries.map(country => countryListTemplate(country)).join('');
+  } else {
+    return error('Too many matches found. Please enter a more specific query!');
+  }
+  renderCountryRows(markup);
+}
+
+function renderCountryRows(markup) {
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearCountryList() {
